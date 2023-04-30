abc81dacdacb7b0ebe85fd3adeda6b998aeba40e
jessie-pastan <jessie.pastan@gmail.com>
2023-04-29 22:32:59 -0400

    Initial Commit

---
+++ Learna/LearnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnaApp.swift
+//  Learna
+//
+//  Created by Jessie P on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
